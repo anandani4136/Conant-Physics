0f86e6cd5915e3181022aa0168957f571b477c6d
anandani4136 <anandani4136@students.d211.org>
2021-08-04 18:54:54 -0500

    css and logo refinements

--- frontend/src/components/LoginPage.js
+++ frontend/src/components/LoginPage.js
@@ -3,12 +3,25 @@ import "./componentsCSS/LoginPageCSS.css"
 
 export default function LoginPage() {
     return (
-        <div id="loginform">
-            <FormHeader title="Login" />
-            <Form />
+        <div>
+            <br></br>
+            <div>
+                <LoginLogo/>
+            </div>
+            <div id="loginform">
+                <FormHeader title="Login" />
+                <Form />
+            </div>
         </div>
     )
 }
+const LoginLogo = props => (
+    <div className="alignRow">
+        <h2 id="loginTitle1">CONANT</h2>
+        <h2 id="loginTitle2">PHYSICS</h2>
+    </div>
+    
+);
 
 const FormHeader = props => (
     <h2 id="headerTitle">{props.title}</h2>
@@ -24,13 +37,13 @@ const Form = props => (
 );
 
 const FormButton = props => (
-    <div id="button" class="row">
+    <div id="button" className="row">
         <button>{props.title}</button>
     </div>
 );
 
 const FormInput = props => (
-    <div class="row">
+    <div className="row">
         <label>{props.description}</label>
         <input type={props.type} placeholder={props.placeholder}/>
     </div>
