f95d94989fdf08779bfeeaacdb875c0fdb070dac
anandani4136 <anandani4136@students.d211.org>
2021-08-09 15:23:36 -0500

    basic navbar

--- frontend/src/components/HomeScreen.js
+++ frontend/src/components/HomeScreen.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import "./componentsCSS/HomeScreenCSS.css"
+import "./componentsCSS/navbar.css"
 import userAuth from '../static/documents/userAuth';
+import {Navbar, Nav, NavDropdown, Container} from 'react-bootstrap';
 
 // export default function LoginPage() {
 //     return (
@@ -31,19 +33,37 @@ export default class HomeScreen extends React.Component {
         // alert(userAuth.auth)
         // if (!window.signedIn) {
         // console.log(userAuth.userAuth)
-        alert(userAuth.value)
+        // alert(userAuth.value)
         
         if (!userAuth.value)  {   
             window.location.href = window.url;
         } else {
-            alert("WELCOME")
+            // alert("WELCOME")
         }
     }
 
 
     render() {
         return (
-            <h1>HE</h1>
+            <>
+                <Navbar bg="primary" variant="dark">
+                    <Container>
+                    <Navbar.Brand href="/home">
+                        <div className="alignRow">
+                            <h2 id="loginTitle3">CONANT</h2>
+                            <h2 id="loginTitle4">PHYSICS</h2>
+                        </div>
+                    </Navbar.Brand>
+                    <Nav className="me-auto">
+                        <Nav.Link className = "color-navlink" href="#home">About</Nav.Link>
+                        <Nav.Link className = "color-navlink" href="#features">Resources</Nav.Link>
+                        <Nav.Link className = "color-navlink" href="#pricing">Contact</Nav.Link>
+                        <Nav.Link className = "color-navlink" href="#pricing">Classes</Nav.Link>
+                        <NavDropdown id = "color-dropdown" title="Projects" ></NavDropdown>
+                    </Nav>
+                    </Container>
+                </Navbar>
+            </>
         );
     }
 }
